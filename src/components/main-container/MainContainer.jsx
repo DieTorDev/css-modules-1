85dd014ba9bd2e37a39b0ea715808eb538460c7d
diegotrazos <diego.torradosanchez@alumnostrazos.net>
2024-06-02 19:55:28 +0200

    update

--- src/components/main-container/MainContainer.jsx
+++ src/components/main-container/MainContainer.jsx
@@ -1,9 +1,10 @@
 import Result from '../result/Result';
 import Summary from '../summary/Summary';
+import style from './main-container.module.css';
 
 const MainContainer = () => {
 	return (
-		<main>
+		<main className={style.main_container}>
 			<Result />
 			<Summary />
 		</main>
